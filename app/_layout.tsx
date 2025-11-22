abf9f1c8edf6771fa05d8844f8c01c1166322640
Quốc Huy Đặng <danghuy0751245@gmail.com>
2025-11-22 16:31:25 +0700

    config navigation

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,9 +1,13 @@
-import { Stack } from 'expo-router';
-import * as SplashScreen from 'expo-splash-screen';
-import { StatusBar } from 'expo-status-bar';
-import { useEffect } from 'react';
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import { NavigationContainer } from "@react-navigation/native";
+import * as SplashScreen from "expo-splash-screen";
+import { useEffect } from "react";
+import HomeScreen from "./pages/HomeScreen";
+import TodoScreen from "./pages/TodoScreen";
 
 SplashScreen.preventAutoHideAsync();
+
+const Tab = createBottomTabNavigator();
 
 export default function RootLayout() {
   useEffect(() => {
@@ -11,11 +15,34 @@ export default function RootLayout() {
   }, []);
 
   return (
-    <>
-      <Stack>
-        <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-      </Stack>
-      <StatusBar style="auto" />
-    </>
+    <NavigationContainer>
+      <Tab.Navigator
+        screenOptions={{
+          tabBarActiveTintColor: "#007AFF",
+          tabBarInactiveTintColor: "#8E8E93",
+          headerShown: false,
+          tabBarStyle: {
+            backgroundColor: "#fff",
+            borderTopWidth: 1,
+            borderTopColor: "#e0e0e0", 
+          },
+        }}
+      >
+        <Tab.Screen
+          name="Home"
+          component={HomeScreen}
+          options={{
+            tabBarLabel: "Home",
+          }}
+        />
+        <Tab.Screen
+          name="Todo"
+          component={TodoScreen}
+          options={{
+            tabBarLabel: "Todo List",
+          }}
+        />
+      </Tab.Navigator>
+    </NavigationContainer>
   );
 }
